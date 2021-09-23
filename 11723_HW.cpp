a01a20fc6bac2e6b6a4ec4ebd0aefda2ccae1511
SoominChung <62084163+SoominChung@users.noreply.github.com>
2021-09-23 12:54:34 +0900

    Update 11723_HW.cpp
    x의 입력 없는 'all', 'empty' 때문에 calc과 x 입력 나누어 받아야한다는 거 몰랐을 때는 case 사용하는게 가독성 나아보였는데,(+case가 속도도 빠르니까) calc과 x 따로 받아야하는 거 알고나니 if else문이 오히려 깔끔했을 수도 있겠다는 생각..

--- 11723_HW.cpp
+++ 11723_HW.cpp
@@ -6,58 +6,72 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
-
+//#include <map> // 0923 시간초과 나서 용량 줄여야하는데, S 원소 x의 범위가 1~20이므로 vector 사용해도 됨
 
 using namespace std;
 
-map<int,bool> S;
+//map<int,bool> S;
+vector<bool> S; // S map -> vector로 수정
+
+const int SIZE = 21; // arr 0부터 시작하지만 우린 idx: 1~20만 사용할 것
 
 int main(){
     string calc;
     int n,x;
 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     //S 생성
-    for(int i=0; i<20;i++){
-        S[i+1] = false;
-    }
+    S.assign(SIZE,false);
+    //for(int i=0; i<20;i++){
+    //    S[i+1] = false;
+    //}
 
     cin>>n;
     while(n--){
-        cin>>calc>>x;
+        cin>>calc; //초기 cin>>calc>>x의 경우 'all', 'empty'의 경우 x가 안들어감. 따라서 x 입력은 case문에서 받아주었음. 
         if(calc == "add"){
-            S[x] = true;
+            calc = "plus";
         }
-        else if(calc == "remove"){
-            S[x] = false;
-        }
-        else if(calc == "check"){
-            if(S[x]){
-                cout<<1;
-            }
-            else{
-                cout<<0;
-            }
-        }
-        else if(calc == "toggle"){
-            if(S[x]){
-                S[x] = false;
-            }
-            else{
+        switch (calc[0]) {
+            case 'p': //add
+                cin>>x;
                 S[x] = true;
+                break;
+                
+            case 'r': //remove
+                cin>>x;
+                S[x] = false;
+                break;
+                
+            case 'c': //check
+                cin>>x;
+
+                if(S[x]){
+                    cout<<1<<'\n';
+                }
+                else{
+                    cout<<0<<'\n';
+                }
+                break;
+                
+            case 't': { //toggle
+                cin>>x;
+                bool tmp = S[x]; // case 문 안에서 새로운 변수 지정할 경우, {} 해줘야함
+                S[x] = !tmp;
+                break;
             }
-        }
-        else if(calc == "all"){
-            for(int i=0; i<20;i++){
-                S[i] = true;
+                
+            case 'a': { //all
+                S.assign(SIZE,true); //for문 대신 assign으로 수정
+                break;
             }
-        }
-        else if(calc == "empty"){
-            for(int i=0; i<20; i++){
-                S[i] = false;
+            case 'e': { //empty
+                S.assign(SIZE,false);
+                break;
             }
         }
     }
-
 
 }
